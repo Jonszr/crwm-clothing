e632395935319e8bba94f8f1fd67a142aaa397b4
Jonszr <s13712651652@gmail.com>
2023-04-20 17:09:06 -0600

    Created shop nested routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./router/home/home.component";
-import Navigation from "./router/navigation/navigation.conponent";
-import Authentication from "./router/authentication/authentication.component";
-import Shop from "./router/shop/shop.component";
-import Checkout from "./router/checkout/checkout.component";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.conponent";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
+        <Route path="shop/*" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
         <Route path="checkout" element={<Checkout />} />
       </Route>
